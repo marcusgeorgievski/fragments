35944516f828aa86e7a4ade10de5b28b265c3cb5
Marcus Georgievski <marcusgeorgievski@outlook.com>
2024-01-27 20:28:03 -0500

    Refactor to use response.js functions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 // src/routes/index.js
 
 const express = require('express');
+const { createSuccessResponse } = require('../response');
 
 // Our authentication middleware
 const { authenticate } = require('../auth');
@@ -31,13 +32,14 @@ router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // Use your own GitHub URL for this!
-    githubUrl: 'https://github.com/marcusgeorgievski/fragments',
-    version,
-  });
+  res.status(200).json(
+    createSuccessResponse({
+      author,
+      // Use your own GitHub URL for this!
+      githubUrl: 'https://github.com/marcusgeorgievski/fragments',
+      version,
+    })
+  );
 });
 
 module.exports = router;
