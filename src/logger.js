906d4b7ffda9a91faa51fb0ba08baa8fd4413ef9
Marcus Georgievski <marcusgeorgievski@outlook.com>
2024-03-20 00:57:43 -0400

    updated logger

--- src/logger.js
+++ src/logger.js
@@ -9,7 +9,7 @@ const options = { level: process.env.LOG_LEVEL || 'info' };
 if (options.level === 'debug') {
   // https://github.com/pinojs/pino-pretty
   options.transport = {
-    // target: 'pino-pretty',
+    target: 'pino-pretty',
     options: {
       colorize: true,
     },
