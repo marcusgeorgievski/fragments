950130c84cdaed0dd064b1347f55d6fcb1774f77
Marcus Georgievski <marcusgeorgievski@outlook.com>
2024-03-21 03:42:45 -0400

    added location header test

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -1,12 +1,16 @@
 const request = require('supertest');
 const crypto = require('crypto');
-
 const app = require('../../src/app');
+
+/**
+ * @typedef {import('../../src/types.js').PostResponse} PostResponse
+ */
 
 describe('POST /v1/fragments', () => {
   test('authenticated users get a fragment by id', async () => {
     const data = Buffer.from([1, 2, 3]);
 
+    /** @type {PostResponse} */
     const res = await request(app)
       .post('/v1/fragments')
       .auth('user1@email.com', 'password1')
@@ -17,9 +21,24 @@ describe('POST /v1/fragments', () => {
     expect(res.statusCode).toBe(201);
   });
 
+  test('expect correct location header', async () => {
+    const data = Buffer.from([1, 2, 3]);
+
+    /** @type {PostResponse} */
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .send(data)
+      .set('Content-Type', 'text/plain')
+      .set('Content-Length', data.length);
+
+    expect(res.header.location.endsWith(`v1/fragments/${res.body.fragment.id}`)).toBe(true);
+  });
+
   test('invalid user not authorized and returns 401', async () => {
     const data = Buffer.from([1, 2, 3]);
 
+    /** @type {PostResponse} */
     const res = await request(app)
       .post('/v1/fragments')
       .auth('user1@emailcom', 'password1')
@@ -31,6 +50,7 @@ describe('POST /v1/fragments', () => {
   });
 
   test('correct owner id returned from post response', async () => {
+    /** @type {PostResponse} */
     const res = await request(app)
       .post('/v1/fragments')
       .auth('user1@email.com', 'password1')
@@ -44,6 +64,7 @@ describe('POST /v1/fragments', () => {
   });
 
   test('invalid type returns HTTP 415 status', async () => {
+    /** @type {PostResponse} */
     const res = await request(app)
       .post('/v1/fragments')
       .auth('user1@email.com', 'password1')
