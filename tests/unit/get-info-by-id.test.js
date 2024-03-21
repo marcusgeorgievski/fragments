fdee4fd65b4197621cf7e8caa52d3552fcd290b7
Marcus Georgievski <marcusgeorgievski@outlook.com>
2024-03-21 04:41:07 -0400

    refactored get-by-id and post tests

--- tests/unit/get-info-by-id.test.js
+++ tests/unit/get-info-by-id.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
-
 const app = require('../../src/app');
+const { postFragment } = require('../utils');
 
 describe('GET /v1/fragments/:id/info', () => {
   test('user can create fragment, get it, and have the same id', async () => {
@@ -8,12 +8,7 @@ describe('GET /v1/fragments/:id/info', () => {
 
     const data = Buffer.from(text);
 
-    const postReq = await request(app)
-      .post('/v1/fragments')
-      .auth('user1@email.com', 'password1')
-      .send(data)
-      .set('Content-Type', 'text/plain')
-      .set('Content-Length', data.length);
+    const postReq = await postFragment(data, 'text/plain');
 
     const postResult = postReq.body.fragment;
 
